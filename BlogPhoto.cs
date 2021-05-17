03f3011d9da0e4cf5d37860357cd1ec67c91cab8
Brian Reeves <1949commander@gmail.com>
2021-05-17 11:06:54 -0700

    Upload Items from C-Sharp Projects
    Files from my C# Live Project Stories

---
+++ BlogPhoto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    [Table("BlogPhotos")]
+    public class BlogPhoto
+    {
+
+        [Key]
+        public int BlogPhotoId { get; set; }
+        public string Title { get; set; }
+        public byte[] Photo { get; set; }
+    }
+    
+}
